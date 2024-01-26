a35b2081d033482f195f84c94c881e16af244063
powerninja <asdyuy23@gmail.com>
2024-01-27 01:01:34 +0900

    元に戻す

--- lib/main.dart
+++ lib/main.dart
@@ -60,66 +60,28 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          const Text("HelloWorld"),
-          const Text("ハロワ"),
-          TextButton(
-            onPressed: () => print("ボタンが押下されたよ"),
-            child: const Text("テキストボタン"),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const [
-              Icon(
-                Icons.favorite,
-                color: Colors.pink,
-                size: 24.0,
-              ),
-              Icon(
-                Icons.audiotrack,
-                color: Colors.green,
-                size: 30.0,
-              ),
-              Icon(
-                Icons.beach_access,
-                color: Colors.blue,
-                size: 36.0,
-              )
-            ],
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //画面の中央に表示されるテキスト
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            // テキストの下に表示されるカウンタ
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
       ),
+      //画面下部のボタン部分
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {print("押したね？")},
-        child: const Icon((Icons.timer)),
-      ),
-      drawer: const Drawer(child: Center(child: Text("Drawer"))),
-      endDrawer: const Drawer(
-        child: Center(child: Text("EndDrawer")),
-      ),
-      // body: Center(
-      //   child: Column(
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>[
-      //       //画面の中央に表示されるテキスト
-      //       const Text(
-      //         'You have pushed the button this many times:',
-      //       ),
-      //       // テキストの下に表示されるカウンタ
-      //       Text(
-      //         '$_counter',
-      //         style: Theme.of(context).textTheme.headlineMedium,
-      //       ),
-      //     ],
-      //   ),
-      // ),
-      // 画面下部のボタン部分
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
